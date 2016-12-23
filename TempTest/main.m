95b9a2b55c46003cade006c8e322a8f66d90627e
leixiang <541150547@qq.com>
2016-12-23 15:08:12 +0800

    Initial Commit

---
+++ TempTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TempTest
+//
+//  Created by 雷祥 on 16/12/23.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
